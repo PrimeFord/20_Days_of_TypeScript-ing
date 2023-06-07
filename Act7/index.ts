91aa4a365cf5e76c42929f5d3c9a63f3b892ea1d
PrimeFord <bashfordbash@gmail.com>
2023-06-07 23:17:55 +0100

    feat: Act7

---
+++ Act7/index.ts
@@ -0,0 +1,64 @@
+//Generic Tuples
+
+function simpleState<T>(initial: T): [() => T, (v: T) => void] {
+  //Generic <>
+  //this function is returning a tuple
+  let val: T = initial;
+  return [
+    () => val,
+    (v: T) => {
+      val = v;
+    },
+  ];
+}
+
+const [st1getter, st1setter] = simpleState(10);
+
+console.log(st1getter());
+st1setter(62);
+console.log(st1getter());
+
+//Overiding inferred generic type
+
+const [st2getter, st2setter] = simpleState<string | null>(null);
+
+console.log(st2getter());
+st2setter("str");
+console.log(st2getter());
+
+//Ranker
+interface Rank<RankItem> {
+  item: RankItem;
+  rank: number;
+}
+
+function ranker<RankItem>(
+  items: RankItem[],
+  rank: (v: RankItem) => number
+): RankItem[] {
+  const ranks: Rank<RankItem>[] = items.map((item) => ({
+    item,
+    rank: rank(item),
+  }));
+  ranks.sort((a, b) => a.rank - b.rank);
+  return ranks.map((rank) => rank.item);
+}
+
+interface Pokemon {
+  name: string;
+  hp: number;
+}
+
+const pokemon: Pokemon[] = [
+  {
+    name: "Balbasaur",
+    hp: 20,
+  },
+  {
+    name: "Megaasaur",
+    hp: 5,
+  },
+];
+
+const ranks = ranker(pokemon, ({ hp }) => hp);
+console.log(ranks);
